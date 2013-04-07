98e25f020beece2a10ab79827790442197494f81
bumer7721 <bumer7721@gmail.com>
2013-04-07 23:00:06 +0300

    Small fix

---
+++ pages/template_figuration/Shuttershot/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div id="casing">
+<div id="ncontent">
+	<div class="npost">
+		<div class="ntitle title">
+			<h2>Не найдено</h2>
+		</div>
+		<div class="nentry">
+	<p>Страница не была найдена..</p>
+		
+		</div>
+	</div>
+
+</div>
+</div>
+<?php get_footer(); ?>
---
+++ pages/template_figuration/Shuttershot/sponsors.php
@@ -0,0 +1,5 @@
+<div class="squarebanner clearfix">
+	<h3 class="sidetitl "> Спонсоры </h3>
+<ul>
+</ul>
+</div>
